4b44fdc7aa4563916d9f04dad82deedbdf0f2c38
unknown <alejandrojzeballos@gmail.com>
2021-07-26 22:48:03 -0400

    {add} adding types of fields and data

--- DataModeling.js
+++ DataModeling.js
@@ -6,6 +6,27 @@ rows as records
 types of objects. 
 standard objects, custom objects, external objects, platform events, and BigObjects.
 
+field types
+Identity	
+A 15-character, case-sensitive field that’s automatically generated for every record. You can find a record’s ID in its URL.
+
+System	
+Read-only fields that provide information about a record from the system, like when the record was created or when it was last changed
+
+Name	
+All records need names so you can distinguish between them. You can use text names or auto-numbered names that automatically increment every time you create a record.
+
+Custom	Fields you create on standard or custom objects are called custom fields.
+
+data-types
+Checkbox
+for fields that are a simple “yes” or “no,” a checkbox field is what you want.
+
+Date or DateTime
+these field types represent dates or date/time combinations, like birthdays or sales milestones.
+
+Formula
+this special field type holds a value that’s automatically calculated based on a formula that you write
 
 
 */
